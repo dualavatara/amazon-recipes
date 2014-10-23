181943ccc0e89e72735d7f266d96b4465c41837d
Konstantin Zhukov <kz@pandra.ru>
2014-10-23 11:06:29 +0400

    recipe for retail-like servers

--- retail/attributes/default.rb
+++ retail/attributes/default.rb
@@ -5,13 +5,3 @@ default[:dynamodb][:dbpath] = "/var/local/dynamodb"
 default[:fpm][:port] = "10000"
 default[:facebook][:app_id] = ""
 default[:facebook][:app_secret] = ""
-default[:layers] = {
-    :web_fe => {
-        :branch => 'master',
-        :domain => 'dirtygram.pandra.ru'
-    },
-    :web_fe_test => {
-        :branch => 'develop',
-        :domain => 'test.dirtygram.pandra.ru'
-    }
-}
